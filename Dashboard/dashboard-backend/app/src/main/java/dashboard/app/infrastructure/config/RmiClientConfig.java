a9bc76b63f5694817e7529eff7c9daac7c6e4859
PoliGallego <juan_gallego82231@elpoli.edu.co>
2026-09-08 15:59:12 -0500

    refactor(database): change used database to supabase

--- Dashboard/dashboard-backend/app/src/main/java/dashboard/app/infrastructure/config/RmiClientConfig.java
+++ Dashboard/dashboard-backend/app/src/main/java/dashboard/app/infrastructure/config/RmiClientConfig.java
@@ -4,7 +4,6 @@ import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-import java.util.Arrays;
 
 import rmi.shared.AuthRmiPort;
 import rmi.shared.PanelRemoteService;
